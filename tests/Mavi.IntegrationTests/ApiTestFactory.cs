10aef7fa50da0b45d63098fb83b7ad5f097c5b08
Hari Om Ahlawat <hariomahlawat@outlook.com>
2026-09-14 07:50:00 +0530

    test(task15): align API factory import limits

--- tests/Mavi.IntegrationTests/ApiTestFactory.cs
+++ tests/Mavi.IntegrationTests/ApiTestFactory.cs
@@ -44,7 +44,8 @@ public sealed class ApiTestFactory : WebApplicationFactory<Program>
                 ["MediaProcessing:FfprobePath"] = "ffprobe",
                 ["MediaProcessing:FfmpegPath"] = "ffmpeg",
                 ["MediaProcessing:ProbeTimeoutSeconds"] = "30",
-                ["VideoImport:MaximumFileSizeBytes"] = "10737418240",
+                ["VideoImport:MaximumFileSizeBytes"] = "3221225472",
+                ["VideoImport:MultipartOverheadBytes"] = "1048576",
                 ["VideoImport:AllowedExtensions:0"] = ".mp4",
             });
         });
